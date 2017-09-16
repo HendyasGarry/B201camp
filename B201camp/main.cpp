1196b0589b0cd8a365f7b9fe996116a8a37e2ae5
User\GARRY <garryhendyas@gmail.com>
2017-09-16 14:20:56 +0700

    udah bisa ambil item

--- B201camp/main.cpp
+++ B201camp/main.cpp
@@ -1,16 +1,23 @@
+#include <iostream>
 #include <time.h>
 #include <SFML/Graphics.hpp>
 
+using namespace std;
+
 int main()
 {
-    srand((unsigned)time(0));
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML tes!");
-    sf::RectangleShape player, mobil;
+    int spawn, screenWidth, screenHeight;
+    screenWidth=400;
+    screenHeight=600;
+    bool isDisplayed=1;
 
-    int spawn;
+    srand((unsigned)time(0));
+    sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "SFML tes!");
+    sf::RectangleShape player, mobil;
+    sf::Text txt;
 
     player.setSize(sf::Vector2f(50,80));
-    player.setPosition(1000, 500);
+    player.setPosition(200, 300);
     player.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
@@ -24,28 +31,33 @@ int main()
 
         window.clear();
 
-        mobil.move(0,0.5f);
+        mobil.move(0,0.2f);
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
             player.move(-1, 0);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
             player.move(1, 0);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
             player.move(0, -1);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
             player.move(0, 1);
-
-        if(player.getGlobalBounds().intersects(mobil.getGlobalBounds())){
-            window.draw("BOOM!");
-        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            window.close();
 
         window.draw(player);
-        if(mobil.getPosition().y<=1080)
-            window.draw(mobil);
+        if(mobil.getPosition().y<=screenHeight)
+        {
+            if(player.getGlobalBounds().intersects(mobil.getGlobalBounds())){
+                std::cout<<"BHRUWAK!"<<endl;
+                isDisplayed=0;
+            }
+            else if (isDisplayed)
+                window.draw(mobil);
+        }
         else {
-                spawn=rand()%1920;
+                spawn=rand()%3+1;
                 mobil.setSize(sf::Vector2f(50,80));
-                mobil.setPosition(spawn, 20);
+                mobil.setPosition(screenWidth/spawn+25, 20);
                 mobil.setFillColor(sf::Color::Green);
         }
         window.display();
